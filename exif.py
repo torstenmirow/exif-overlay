5276fe84eb5f424bbd9f209f539b6ebdb3f1f4eb
mirow-mms <mirow@mediamarktsaturn.com>
2022-02-09 03:17:02 +0100

    first final working version

--- exif.py
+++ exif.py
@@ -7,7 +7,7 @@ from PIL.ExifTags import TAGS, GPSTAGS
 from utils import DictX
 
 
-def generate_exif_dict(filepath):
+def generate_exif_dict(filepath, spinner):
     try:
 
         image = Image.open(filepath)
@@ -19,7 +19,7 @@ def generate_exif_dict(filepath):
         exif_data = {}
 
         if exif_data_pil is None:
-            print('Sorry, image has no exif data.')
+            spinner.fail('Sorry, image `%s` has no exif data.' % image.filename)
         else:
             for key, val in exif_data_pil.items():
                 if key in available_tags:
@@ -52,63 +52,63 @@ def _derationalize(rational):
 def _process_exif_dict(exif_dict):
     date_format = "%Y:%m:%d %H:%M:%S"
 
-    if exif_dict["DateTime"]["raw"] is not None:
+    if exif_dict.get("DateTime") is not None and exif_dict["DateTime"]["raw"] is not None:
         exif_dict["DateTime"]["processed"] = \
             datetime.datetime.strptime(exif_dict["DateTime"]["raw"], date_format)
 
-    if exif_dict["DateTimeOriginal"]["raw"] is not None:
+    if exif_dict.get("DateTimeOriginal") is not None and exif_dict["DateTimeOriginal"]["raw"] is not None:
         exif_dict["DateTimeOriginal"]["processed"] = \
             datetime.datetime.strptime(exif_dict["DateTimeOriginal"]["raw"], date_format)
 
-    if exif_dict["DateTimeDigitized"]["raw"] is not None:
+    if exif_dict.get("DateTimeDigitized") is not None and exif_dict["DateTimeDigitized"]["raw"] is not None:
         exif_dict["DateTimeDigitized"]["processed"] = \
             datetime.datetime.strptime(exif_dict["DateTimeDigitized"]["raw"], date_format)
 
-    if exif_dict["FNumber"]["raw"] is not None:
+    if exif_dict.get("FNumber") is not None and exif_dict["FNumber"]["raw"] is not None:
         exif_dict["FNumber"]["processed"] = \
             _derationalize(exif_dict["FNumber"]["raw"])
 
-    if exif_dict["FNumber"]["processed"] is not None:
+    if exif_dict.get("FNumber") is not None and exif_dict["FNumber"]["processed"] is not None:
         exif_dict["FNumber"]["processed"] = \
             "f{}".format(exif_dict["FNumber"]["processed"])
 
-    if exif_dict["MaxApertureValue"]["raw"] is not None:
+    if exif_dict.get("MaxApertureValue") is not None and exif_dict["MaxApertureValue"]["raw"] is not None:
         exif_dict["MaxApertureValue"]["processed"] = \
             _derationalize(exif_dict["MaxApertureValue"]["raw"])
 
-    if exif_dict["MaxApertureValue"]["processed"] is not None:
+    if exif_dict.get("MaxApertureValue") is not None and exif_dict["MaxApertureValue"]["processed"] is not None:
         exif_dict["MaxApertureValue"]["processed"] = \
             "f{:2.1f}".format(exif_dict["MaxApertureValue"]["processed"])
 
-    if exif_dict["FocalLength"]["raw"] is not None:
+    if exif_dict.get("FocalLength") is not None and exif_dict["FocalLength"]["raw"] is not None:
         exif_dict["FocalLength"]["processed"] = \
             _derationalize(exif_dict["FocalLength"]["raw"])
 
-    if exif_dict["FocalLength"]["processed"] is not None:
+    if exif_dict.get("FocalLength") is not None and exif_dict["FocalLength"]["processed"] is not None:
         exif_dict["FocalLength"]["processed"] = \
             "{}mm".format(exif_dict["FocalLength"]["processed"])
 
-    if exif_dict["XResolution"]["raw"] is not None:
+    if exif_dict.get("XResolution") is not None and exif_dict["XResolution"]["raw"] is not None:
         exif_dict["XResolution"]["processed"] = \
             int(_derationalize(exif_dict["XResolution"]["raw"]))
 
-    if exif_dict["YResolution"]["raw"] is not None:
+    if exif_dict.get("YResolution") is not None and exif_dict["YResolution"]["raw"] is not None:
         exif_dict["YResolution"]["processed"] = \
             int(_derationalize(exif_dict["YResolution"]["raw"]))
 
-    if exif_dict["ExposureTime"]["raw"] is not None:
+    if exif_dict.get("ExposureTime") is not None and exif_dict["ExposureTime"]["raw"] is not None:
         exif_dict["ExposureTime"]["processed"] = \
             _derationalize(exif_dict["ExposureTime"]["raw"])
 
-    if exif_dict["ExposureTime"]["processed"] is not None:
+    if exif_dict.get("ExposureTime") is not None and exif_dict["ExposureTime"]["processed"] is not None:
         exif_dict["ExposureTime"]["processed"] = \
             str(Fraction(exif_dict["ExposureTime"]["processed"]).limit_denominator(8000))
 
-    if exif_dict["ExposureBiasValue"]["raw"] is not None:
+    if exif_dict.get("ExposureBiasValue") is not None and exif_dict["ExposureBiasValue"]["raw"] is not None:
         exif_dict["ExposureBiasValue"]["processed"] = \
             _derationalize(exif_dict["ExposureBiasValue"]["raw"])
 
-    if exif_dict["ExposureBiasValue"]["processed"] is not None:
+    if exif_dict.get("ExposureBiasValue") is not None and exif_dict["ExposureBiasValue"]["processed"] is not None:
         exif_dict["ExposureBiasValue"]["processed"] = \
             "{} EV".format(exif_dict["ExposureBiasValue"]["processed"])
 
--- main.py
+++ main.py
@@ -4,50 +4,55 @@ import os
 import pathlib
 import sys
 
-from PIL import Image, ImageFont, ImageDraw, ImageFilter
-from colorama import Fore, Style, init
-from yaspin import yaspin
+from PIL import Image, ImageFont, ImageDraw, ImageFilter, ImageColor
+from colorama import init
+from halo import Halo
 
 from exif import generate_exif_dict
-# spinner = Halo(text='Processing', spinner='dots', color='cyan')
 from utils import dotdict, DictX
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('image_paths', type=pathlib.Path, nargs='?', help='Path to image or folder')
-parser.add_argument('-f', '--font', type=pathlib.Path, help='Path to OTF / TTF font', default="./fonts/SF-Pro-Display-Medium.otf")
+parser.add_argument('-f', dest='font', type=pathlib.Path, help='Path to OTF / TTF font', default="./fonts/SF-Pro-Display-Medium.otf",
+                    metavar='./fonts/SF-Pro-Display-Medium.otf')
+parser.add_argument('-t', dest='transparency', type=int, help='Background transparency 0-100', default="15", metavar='15')
+parser.add_argument('-c', dest='color', type=str, help='Text color in HEX', default="#ffffff", metavar='"#ffffff"')
+parser.add_argument('-b', dest='background', type=str, help='Box color in HEX', default="#000000", metavar='"#000000"')
+parser.add_argument('-p', dest='position', help='Position of exif infos', default="left", choices=['left', 'center', 'right'], metavar='left')
+parser.add_argument('-o', dest='output', type=pathlib.Path, help='Output folder', default=None, metavar='/Desktop/img')
 
 args = parser.parse_args()
-print(args)
 
-'''
-    global
-    spaces in px
-'''
+# Globals
 SPACE_FROM_BOTTOM = 60
 SPACE_FROM_LEFT = 80
 SPACE_BETWEEN_BOXES = 40
 INCLUDED_EXTENSIONS = ['jpg', 'jpeg', 'png']
+POSITION = args.position
+if not args.output:
+    if os.path.isdir(args.image_paths):
+        PATH = args.image_paths
+    else:
+        PATH = os.path.join(os.path.dirname(args.image_paths), 'output')
+else:
+    PATH = os.path.abspath(args.output)
 
-'''
-    BOX
-'''
-# Degree of transparency, 0-100%
-BOX_TRANSPARENCY = 15
+# Box
+BOX_TRANSPARENCY = args.transparency
 BOX_PADDING = dotdict({"top": 20, "right": 50, "bottom": 35, "left": 50})
+BOX_COLOR = ImageColor.getrgb(args.background)
 
 BOX_OPACITY = int(255 * (BOX_TRANSPARENCY / 100))
+BOX_RGBA = (BOX_COLOR + (BOX_OPACITY,))
 
-'''
-    Text
-'''
-# FONT_SIZE = 125
-# as percent
+# Text
 FONT_SIZE = 2.3
-FONT_ITALIC = ImageFont.truetype("./fonts/SF-Pro-Display-LightItalic.otf", 125)
 TEXT_TRANSPARENCY = 100
+TEXT_COLOR = ImageColor.getrgb(args.color)
 
 TEXT_OPACITY = int(255 * (BOX_TRANSPARENCY / 100))
+TEXT_RGBA = (TEXT_COLOR + (TEXT_OPACITY,))
 
 EXIF_TAGS_TO_PRINT = {
     "Model": "Model",
@@ -60,120 +65,40 @@ EXIF_TAGS_TO_PRINT = {
 
 init()
 
-spinner = yaspin(text="Processing images", color="cyan")
-
-
-class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    ITALIC = '\033[3m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
+spinner = Halo(text='Loading..', spinner='bouncingBar', text_color='cyan')
 
 
 # save image with high quality and same ICC Profile
 def _save_image(img):
-    filepath = os.path.dirname(img.filename)
-    filename = os.path.basename(img.filename)
-    new_path = os.path.join(filepath, 'EXIF')
-    if not os.path.exists(new_path):
-        os.makedirs(new_path)
+    basename = os.path.basename(img.filename)
+    filename_parts = os.path.splitext(basename)
+    filename = filename_parts[0] + '.exif' + filename_parts[1]
+    if not os.path.exists(PATH):
+        spinner.fail('Output path "%s" does not exist!' % PATH)
 
-    fullpath = os.path.join(filepath, 'EXIF', filename)
+    fullpath = os.path.join(PATH, filename)
+
     img.save(fullpath, quality=100, subsampling=0, format='JPEG', icc_profile=img.info.get('icc_profile', ''))
-    # spinner.write(' ✔ Saved %s to %s' % (img.filename, fullpath))
 
 
-def draw_image(img, exif_info, count, current):
-    # spinner.text(f"Processing images {count}/{current}")
-    font_size = round(img.height * FONT_SIZE / 100)
-    font = ImageFont.truetype(str(args.font), font_size)
-    text_size_space = font.getsize(' ')
+def _draw_blurred_background(img, drawing_data):
+    image = img
+    mask = Image.new('L', image.size, 0)
+    draw = ImageDraw.Draw(mask)
 
-    image_for_blur = img
+    for item, data in drawing_data.items():
+        draw.rounded_rectangle(data.rectangle.position, fill=255, radius=7)
 
-    rectangle_y = image_for_blur.height - SPACE_FROM_BOTTOM
+    blurry_image = image.filter(ImageFilter.GaussianBlur(30))
+    img.paste(blurry_image, mask=mask)
 
-    box_padding_percent = dotdict(
-        {
-            "top": int(font_size * 16 / 100),
-            "right": int(font_size * 40 / 100),
-            "bottom": int(font_size * 28 / 100),
-            "left": int(font_size * 40 / 100)
-        }
-    )
 
-    mask = Image.new('L', image_for_blur.size, 0)
-    draw_blurred = ImageDraw.Draw(mask)
-    for name, value in reversed(exif_info.items()):
-        if name == 'Exposure':
-            value = "%ss" % str(value)
-        elif name == 'Aperture':
-            value = 'f%s' % str(value)
-        else:
-            value = str(value)
-
-        name = "%s:" % str(name)
-
-        text_size_description = font.getsize(name)
-        text_size_value = font.getsize(value)
-        rectangle_height = text_size_description[1] + box_padding_percent.top + box_padding_percent.bottom
-        rectangle_width = text_size_description[0] + text_size_value[0] + text_size_space[0] + box_padding_percent.left + box_padding_percent.right
-        rectangle_y = rectangle_y - rectangle_height
-        rectangle_x = 0 + SPACE_FROM_LEFT
-        # rectangle_x = int(img.width / 2)
-
-        rectangle_position = [
-            rectangle_x,
-            rectangle_y,
-            rectangle_x + rectangle_width,
-            rectangle_y + rectangle_height,
-        ]
-
+def _draw_text(img, drawing_data):
+    draw = ImageDraw.Draw(img, "RGBA")
+    for item, data in drawing_data.items():
         # draw.rectangle(rectangle_position, fill=(255, 255, 255, BOX_OPACITY))
 
-        draw_blurred.rounded_rectangle(rectangle_position, fill=255, radius=7)
-        rectangle_y = rectangle_y - SPACE_BETWEEN_BOXES
-
-    blurred_img = image_for_blur.filter(ImageFilter.GaussianBlur(30))
-    img.paste(blurred_img, mask=mask)
-
-    draw_text = ImageDraw.Draw(img, "RGBA")
-    rectangle_y = img.height - SPACE_FROM_BOTTOM
-    for name, value in reversed(exif_info.items()):
-        if name == 'Exposure':
-            value = "%ss" % str(value)
-        elif name == 'Aperture':
-            value = 'f%s' % str(value)
-        else:
-            value = str(value)
-
-        name = "%s:" % str(name)
-
-        text_size_description = font.getsize(name)
-        text_size_value = font.getsize(value)
-        rectangle_height = text_size_description[1] + box_padding_percent.top + box_padding_percent.bottom
-        rectangle_width = text_size_description[0] + text_size_value[0] + text_size_space[0] + box_padding_percent.left + box_padding_percent.right
-        rectangle_y = rectangle_y - rectangle_height
-        rectangle_x = 0 + SPACE_FROM_LEFT
-        # rectangle_x = int(img.width / 2)
-
-        rectangle_position = [
-            rectangle_x,
-            rectangle_y,
-            rectangle_x + rectangle_width,
-            rectangle_y + rectangle_height,
-        ]
-
-        # draw.rectangle(rectangle_position, fill=(255, 255, 255, BOX_OPACITY))
-
-        draw_text.rounded_rectangle(rectangle_position, fill=(255, 255, 255, BOX_OPACITY), radius=7)
+        draw.rounded_rectangle(data.rectangle.position, fill=BOX_RGBA, radius=7)
         #
 
         # # drop shadow
@@ -191,24 +116,31 @@ def draw_image(img, exif_info, count, current):
         # icon.thumbnail((font_size, font_size), Image.ANTIALIAS)
         #
         # img.paste(icon, (rectangle_x + BOX_PADDING.left, rectangle_y + BOX_PADDING.top), icon)
-        draw_text.text((rectangle_x + box_padding_percent.left, rectangle_y + box_padding_percent.top), name, (255, 255, 255, 255), font=font)
+        draw.text(data.text_description.position, data.text_description.text, TEXT_RGBA, font=data.text_description.font)
 
-        draw_text.text(
-            (rectangle_x + box_padding_percent.left + text_size_description[0] + text_size_space[0], rectangle_y + box_padding_percent.top),
-            str(value),
-            (255, 255, 255, 255),
-            font=font
+        draw.text(
+            data.text_value.position,
+            data.text_value.text,
+            TEXT_RGBA,
+            font=data.text_value.font
         )
-        rectangle_y = rectangle_y - SPACE_BETWEEN_BOXES
 
+
+def draw_image(img, exif_info):
+    drawing_data = _generate_draw_data(img, exif_info)
+    _draw_blurred_background(img, drawing_data)
+    _draw_text(img, drawing_data)
     _save_image(img)
 
 
 def _generate_draw_data(img, exif_info):
     font_size = round(img.height * FONT_SIZE / 100)
+    space_left = round(img.height * (SPACE_FROM_LEFT / img.height * 100) / 100)
+    space_between = round(img.height * (SPACE_BETWEEN_BOXES / img.height * 100) / 100)
+    space_bottom = round(img.height * (SPACE_FROM_BOTTOM / img.height * 100) / 100)
     font = ImageFont.truetype(str(args.font), font_size)
     text_size_space = font.getsize(' ')
-    rectangle_y = img.height - SPACE_FROM_BOTTOM
+    rectangle_y = img.height - space_bottom
     box_padding_percent = dotdict(
         {
             "top": int(font_size * 16 / 100),
@@ -217,7 +149,7 @@ def _generate_draw_data(img, exif_info):
             "left": int(font_size * 40 / 100)
         }
     )
-    virtual_drawing = DictX({})
+    draw_data = DictX({})
     for name, value in reversed(exif_info.items()):
         if name == 'Exposure':
             value = "%ss" % str(value)
@@ -233,8 +165,7 @@ def _generate_draw_data(img, exif_info):
         rectangle_height = text_size_description[1] + box_padding_percent.top + box_padding_percent.bottom
         rectangle_width = text_size_description[0] + text_size_value[0] + text_size_space[0] + box_padding_percent.left + box_padding_percent.right
         rectangle_y = rectangle_y - rectangle_height
-        rectangle_x = 0 + SPACE_FROM_LEFT
-        # rectangle_x = int(img.width / 2)
+        rectangle_x = 0 + space_left
 
         rectangle_position = [
             rectangle_x,
@@ -243,40 +174,48 @@ def _generate_draw_data(img, exif_info):
             rectangle_y + rectangle_height,
         ]
 
-        virtual_drawing[name] = DictX({
+        draw_data[name] = DictX({
             "rectangle": DictX({
                 "width": rectangle_width,
                 "height": rectangle_height,
-                "position": rectangle_position
+                "position": rectangle_position,
             }),
             "text_description": DictX({
                 "width": text_size_description[0],
                 "height": text_size_description[1],
                 "position": (rectangle_x + box_padding_percent.left, rectangle_y + box_padding_percent.top),
-                "text": str(name)
+                "text": str(name),
+                "font": font
             }),
             "text_value": DictX({
                 "width": text_size_value[0],
                 "height": text_size_value[1],
                 "position": (rectangle_x + box_padding_percent.left + text_size_description[0] + text_size_space[0], rectangle_y + box_padding_percent.top),
-                "text": str(value)
+                "text": str(value),
+                "font": font
             })
         })
 
-        rectangle_y = rectangle_y - SPACE_BETWEEN_BOXES
+        rectangle_y = rectangle_y - space_between
+
+    return draw_data
 
 
 def custom_exif(filepath):
-    exif_data = generate_exif_dict(filepath)
+    exif_data = generate_exif_dict(filepath, spinner)
     if exif_data is None:
         return
 
     exif_info = {}
 
     for name, value in EXIF_TAGS_TO_PRINT.items():
-        exif_value = exif_data[value]['processed']
-        if exif_value is not None:
-            exif_info[name] = exif_data[value]['processed']
+        if exif_data.get(value) is not None:
+            exif_value = exif_data[value]['processed']
+            if exif_value is not None:
+                exif_info[name] = exif_data[value]['processed']
+
+    if len(exif_info.values()) == 0:
+        return None
 
     return exif_info
 
@@ -300,41 +239,36 @@ def read_image():
     if os.path.isdir(filepath):
         image_list = get_list_of_images(filepath)
         number_of_operations = len(image_list)
-        print(''.join([
-            Fore.YELLOW + "Parsing the following images from ",
-            color.ITALIC + Fore.BLUE + "\'%s:\'\n" % filepath + color.END,
-            *list(map(lambda x: color.ITALIC + Fore.RED + "  * %s\n" % x[1] + color.END, enumerate(image_list))),
-            Style.RESET_ALL
-        ]))
+        spinner.start('Parsing %i images' % number_of_operations)
 
-        for index, img_path in enumerate(image_list):
+        for img_path in image_list:
             fullpath = os.path.join(filepath, img_path)
             image = Image.open(fullpath)
             exif_info = custom_exif(fullpath)
             if exif_info is None:
-                return
-            draw_image(image, exif_info, len(image_list), index + 1)
-        spinner.ok("✅ ")
+                spinner.warn('%s contains no exif data' % image.filename)
+                number_of_operations -= 1
+                spinner.start('Parsing %i images' % number_of_operations)
+                continue
+            draw_image(image, exif_info)
+        spinner.succeed("Saved %i images to %s." % (number_of_operations, args.output))
 
     elif os.path.isfile(filepath):
-
-        spinner.write(''.join([
-            Fore.YELLOW + "Parsing image ",
-            color.ITALIC + Fore.BLUE + "%s" % filepath + color.END,
-            Fore.YELLOW + ".",
-            Style.RESET_ALL
-        ]))
+        spinner.start('Parsing 1 image')
 
         image = Image.open(filepath)
         exif_info = custom_exif(filepath)
-        draw_image(image, exif_info, 1, 1)
-        spinner.ok("✅ done")
+        if exif_info is None:
+            spinner.fail('%s contains no exif data' % image.filename)
+            return
+        draw_image(image, exif_info)
+        spinner.succeed("Saved 1 image to %s." % args.output)
 
     else:
-        print("It is a special file (socket, FIFO, device file)")
-
-    spinner.stop()
+        spinner.warn("Unknown file.")
 
 
 if __name__ == '__main__':
+    spinner.start('Loading...')
     read_image()
+    spinner.stop()
